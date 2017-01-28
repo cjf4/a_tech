e05aba6f8a4f5ade97e0b68788673ef59a487020
Christopher Fenton <christopherfenton@MacBook-Air.local>
2017-01-28 16:12:36 -0500

    3 survey models

--- a_tech/surveys/models.py
+++ a_tech/surveys/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from django.utils import timezone
 
+class Survey(models.Model):
+    survey_name = models.CharField(max_length=50)
+    survey_description = models.CharField(max_length=1000)
+    date_created = models.DateTimeField('date created')
+    date_updated = models.DateTimeField('date updated', null = True)
+    def __str__(self):
+        return self.survey_name
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    date_created = models.DateTimeField('date created', default = timezone.now)
     def __str__(self):
         return self.question_text
+
+class QuestionOrder(models.Model):
+    question_id = models.ForeignKey(Question, on_delete = models.CASCADE)
+    survey_id = models.ForeignKey(Survey, on_delete = models.CASCADE)
+    order = models.IntegerField()
